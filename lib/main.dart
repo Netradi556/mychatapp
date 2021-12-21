866539044dbca7468959f1c78add8c4eb560e252
Gekitenius <gekitenius.yogi@gmail.com>
2021-12-21 19:21:52 +0900

    Complete Auth ChatApp

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
+  //事前処理
+  WidgetsFlutterBinding.ensureInitialized();
   // Firebase初期化
   await Firebase.initializeApp();
   runApp(ChatApp());
@@ -27,26 +29,82 @@ class ChatApp extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // メッセージ表示用
+  String infoText = '';
+
+  // 入力したメールアドレス・パスワード
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          // チャット画面に繊維＋ログイン画面を破棄
-          ElevatedButton(
-              onPressed: () async {
-                await Navigator.of(context)
-                    .pushReplacement(MaterialPageRoute(builder: (context) {
-                  return ChatPage();
-                }));
-              },
-              child: Text('ログイン'))
-        ],
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // メールアドレス入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'メールアドレス'),
+                onChanged: (String value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              // パスワード入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'パスワード'),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                // メッセージ表示
+                child: Text(infoText),
+              ),
+              Container(
+                width: double.infinity,
+                // ユーザー登録ボタン
+                child: ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        // メール・パスワードでユーザー登録
+                        final FirebaseAuth auth = FirebaseAuth.instance;
+                        await auth.createUserWithEmailAndPassword(
+                            email: email, password: password);
+                        // ユーザー登録に成功した場合
+                        // チャット画面に繊維＋ログイン画面を破棄
+                        await Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) {
+                          return ChatPage();
+                        }));
+                      } catch (e) {
+                        // ユーザー登録に失敗した場合
+                        setState(() {
+                          infoText = "登録に失敗しました: ${e.toString()}";
+                        });
+                      }
+                    },
+                    child: Text('ユーザー登録')),
+              ),
+            ],
+          ),
+        ),
       ),
-    ));
+    );
   }
 }
 
