2b55742d1037058e361b6558eb02542a7145afe8
Gekitenius <gekitenius.yogi@gmail.com>
2021-12-21 14:25:31 +0900

    FirebaseStore create document OK

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   // Firebase初期化
@@ -14,11 +15,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyAuthPage(),
+      home: MyFirestorePage(),
+    );
+  }
+}
+
+class MyFirestorePage extends StatefulWidget {
+  @override
+  _MyFirestorePageState createState() => _MyFirestorePageState();
+}
+
+class _MyFirestorePageState extends State<MyFirestorePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () async {
+                  // ドキュメント作成
+                  await FirebaseFirestore.instance
+                      .collection('users') // コレクションID
+                      .doc('id_abc') //ドキュメントID
+                      .set({'name': '鈴木', 'age': 40});
+                },
+                child: Text('コレクション＋ドキュメント作成'))
+          ],
+        ),
+      ),
     );
   }
 }
