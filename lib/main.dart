8f77ed9c923092105aacfccd7b7792d88c79b175
Gekitenius <gekitenius.yogi@gmail.com>
2021-12-23 00:21:59 +0900

    MyChatApp Provider導入

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,60 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// ユーザー情報の受け渡しを行うためのProvicer
+final userProvider = StateProvider.autoDispose((ref) {
+  return FirebaseAuth.instance.currentUser;
+});
+
+// エラー情報の受け渡しを行うためのProvider
+// ※ autoDisposeをつけることで自動的に値をリセットできます
+final infoTextProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// メールアドレスの受け渡しを行うためのProvider
+// ※ autoDisposeをつけることで自動的に値をリセットできます
+final emailProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// パスワードの受け渡しを行うためのProvider
+// ※ autoDisposeをつけることで自動的に値をリセットできます
+final passwordProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// メッセージの受け渡しを行うためのProvider
+// ※ autoDisposeをつけることで自動的に値をリセットできます
+final messageTextProvider = StateProvider.autoDispose((ref) {
+  return '';
+});
+
+// StreamProviderを使うことでStreamも扱うことができる
+// ※ autoDisposeをつけることで自動的に値をリセットできます
+final postsQueryProvider = StreamProvider.autoDispose((ref) {
+  return FirebaseFirestore.instance
+      .collection('posts')
+      .orderBy('date')
+      .snapshots();
+});
 
 Future<void> main() async {
   //事前処理
   WidgetsFlutterBinding.ensureInitialized();
   // Firebase初期化
   await Firebase.initializeApp();
-  runApp(ChatApp());
+  runApp(
+      // Riverpodでデータを受け渡しできる状態にする
+      ProviderScope(
+    child: ChatApp(),
+  ));
 }
 
 // 更新可能なデータ
+// Riverpodでは使用しない？？？
 class UserState extends ChangeNotifier {
   User? user;
 
@@ -30,41 +73,51 @@ class ChatApp extends StatelessWidget {
   // ユーザーの情報を管理するデータ
   final UserState userState = UserState();
 
+  // Providerは最初に呼び出すWidgetのbuild以下をラップしていた
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<UserState>(
-        create: (context) => UserState(),
-        child: MaterialApp(
-          //アプリ名
-          title: 'ChatApp',
-          theme: ThemeData(
-            //テーマカラー
-            primarySwatch: Colors.blue,
-          ),
-          //ログイン画面を表示
-          home: LoginPage(),
-        ));
+    return MaterialApp(
+      //アプリ名
+      title: 'ChatApp',
+      theme: ThemeData(
+        //テーマカラー
+        primarySwatch: Colors.blue,
+      ),
+      //ログイン画面を表示
+      home: LoginPage(),
+    );
   }
 }
 
 // ログイン画面用Widget
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
 
-class _LoginPageState extends State<LoginPage> {
+// class LoginPage extends StatefulWidget {
+//  @override
+//   _LoginPageState createState() => _LoginPageState();
+// }
+
+// ConsumerWidgetでProviderから値を受け渡す
+// ProviderではState<LoginPage> を継承していたが、不要になった
+class LoginPage extends ConsumerWidget {
   // メッセージ表示用
-  String infoText = '';
+  // Providerではここに記述していた
+  // String infoText = '';
 
   // 入力したメールアドレス・パスワード
-  String email = '';
-  String password = '';
+  // String email = '';
+  // String password = '';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    // メッセージ表示用
+    final infoText = watch(infoTextProvider).state;
+    // 入力したメールアドレス・パスワード
+    final email = watch(emailProvider).state;
+    final password = watch(passwordProvider).state;
+
     // ユーザー情報を受け取る
-    final UserState userState = Provider.of<UserState>(context);
+    // Riverpodの導入に伴い不要
+    // final UserState userState = Provider.of<UserState>(context);
 
     return Scaffold(
       body: Center(
@@ -77,9 +130,13 @@ class _LoginPageState extends State<LoginPage> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'メールアドレス'),
                 onChanged: (String value) {
-                  setState(() {
-                    email = value;
-                  });
+                  // Riverpod導入に伴ってsetState()が不要になった
+                  //setState(() {
+                  //  email = value;
+                  //});
+
+                  // Providerから値を更新
+                  context.read(emailProvider).state = value;
                 },
               ),
               // パスワード入力
@@ -87,9 +144,12 @@ class _LoginPageState extends State<LoginPage> {
                 decoration: InputDecoration(labelText: 'パスワード'),
                 obscureText: true,
                 onChanged: (String value) {
-                  setState(() {
-                    password = value;
-                  });
+                  //  setState(() {
+                  //    password = value;
+                  //  });
+
+                  // Providerから値を更新
+                  context.read(passwordProvider).state = value;
                 },
               ),
               Container(
@@ -109,7 +169,9 @@ class _LoginPageState extends State<LoginPage> {
                             await auth.createUserWithEmailAndPassword(
                                 email: email, password: password);
                         // ユーザー情報を更新
-                        userState.setUser(result.user!);
+                        // userState.setUser(result.user!); Riverpod導入に伴って不要に
+                        context.read(userProvider).state = result.user;
+
                         // ユーザー登録に成功した場合
                         // チャット画面に繊維＋ログイン画面を破棄
                         await Navigator.of(context).pushReplacement(
@@ -118,9 +180,12 @@ class _LoginPageState extends State<LoginPage> {
                         }));
                       } catch (e) {
                         // ユーザー登録に失敗した場合
-                        setState(() {
-                          infoText = "登録に失敗しました: ${e.toString()}";
-                        });
+                        // Provider導入に伴って不要に
+                        // setState(() {
+                        //  infoText = "登録に失敗しました: ${e.toString()}";
+                        // });
+                        context.read(infoTextProvider).state =
+                            "登録に失敗しました: ${e.toString()}";
                       }
                     },
                     child: Text('ユーザー登録')),
@@ -136,10 +201,10 @@ class _LoginPageState extends State<LoginPage> {
                       try {
                         // メール・パスワードでログイン
                         final FirebaseAuth auth = FirebaseAuth.instance;
-                        final result = await auth.signInWithEmailAndPassword(
+/*                         final result = await auth.signInWithEmailAndPassword(
                             email: email, password: password);
                         // ユーザー情報を更新
-                        userState.setUser(result.user!);
+                        userState.setUser(result.user!); */
                         // ログインに成功した場合
                         // チャット画面に遷移＋ログイン画面を破棄
                         await Navigator.of(context).pushReplacement(
@@ -148,9 +213,12 @@ class _LoginPageState extends State<LoginPage> {
                         }));
                       } catch (e) {
                         // ログインに失敗した場合
-                        setState(() {
+                        // Providerから値を更新
+                        context.read(infoTextProvider).state =
+                            "ログインに失敗しました${e.toString()}";
+/*                         setState(() {
                           infoText = "ログインに失敗しました${e.toString()}";
-                        });
+                        }); */
                       }
                     },
                     child: Text('ログイン')),
@@ -164,7 +232,7 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 // チャット画面用Widget
-class ChatPage extends StatelessWidget {
+class ChatPage extends ConsumerWidget {
   // コンストラクタを作成して、引数からユーザー情報を受け取れるようにする
   // ChatPage(this.user);
   // →Providerの導入によって、コンストラクタ＋引数で状態を受け取らなくて良くなった
@@ -173,10 +241,10 @@ class ChatPage extends StatelessWidget {
   // final User user; Providerなしのパターンの記述
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
     // ユーザー情報をbuildメソッド内で受け取る
-    final UserState userState = Provider.of<UserState>(context);
-    final User user = userState.user!;
+    final User user = watch(userProvider).state!;
+    final AsyncValue<QuerySnapshot> asyncPostsQuery = watch(postsQueryProvider);
 
     return Scaffold(
         appBar: AppBar(
@@ -205,10 +273,49 @@ class ChatPage extends StatelessWidget {
             Expanded(
                 // FutureBuilder
                 // 非同期処理の結果を元にWidgetを作れる
-                child: StreamBuilder<QuerySnapshot>(
-              // 投稿メッセージ一覧を取得（非同期処理）
-              // 投稿日時でソート
-              stream: FirebaseFirestore.instance
+                // child: StreamBuilder<QuerySnapshot>( Riverpod導入に伴って変更
+                child: asyncPostsQuery.when(
+                    // 値が取得できたとき
+                    data: (QuerySnapshot query) {
+              // List表示
+              return ListView(
+                  // クエリで情報を取得してCardWidgetを作成する
+                  children: query.docs.map((document) {
+                return Card(
+                  child: ListTile(
+                    title: Text(document['text']),
+                    subtitle: Text(document['email']),
+                    // ログイン中のアカウントが投稿したものであれば、削除ボタンを表示
+                    trailing: document['email'] == user.email
+                        ? IconButton(
+                            onPressed: () async {
+                              // 投稿メッセージのドキュメントを削除
+                              await FirebaseFirestore.instance
+                                  .collection('posts')
+                                  .doc(document.id)
+                                  .delete();
+                            },
+                            icon: Icon(Icons.delete))
+                        : null,
+                  ),
+                );
+              }).toList());
+            },
+                    // 値が読込中のとき
+                    loading: () {
+              return Center(
+                child: Text('読込中...'),
+              );
+            },
+                    // 値の取得に失敗したとき
+                    error: (e, stackTrace) {
+              return Center(
+                child: Text(e.toString()),
+              );
+            })
+                // 投稿メッセージ一覧を取得（非同期処理）
+                // 投稿日時でソート
+/*               stream: FirebaseFirestore.instance
                   .collection('posts')
                   .orderBy('date')
                   .snapshots(),
@@ -245,8 +352,8 @@ class ChatPage extends StatelessWidget {
                 return Center(
                   child: Text('読込中'),
                 );
-              },
-            ))
+              }, */
+                )
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -262,7 +369,7 @@ class ChatPage extends StatelessWidget {
   }
 }
 
-class AddPostPage extends StatefulWidget {
+/* class AddPostPage extends StatefulWidget {
   // ↓Providerの導入に伴ってコンストラクタ＋引数で状態を受け取らなくて良い
   // 引数からユーザー情報を受け取る
   // AddPostPage(this.user);
@@ -271,20 +378,17 @@ class AddPostPage extends StatefulWidget {
 
   @override
   _AddPostPageState createState() => _AddPostPageState();
-}
+} */
 
 // 投稿画面用Widget
-class _AddPostPageState extends State<AddPostPage> {
-  //入力した投稿メッセージ
-  String messageText = '';
-
+class AddPostPage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
     // class AddPostPage extends StatefulWidgetの代わりに
     // build()内でユーザー情報を受け取る
 
-    final UserState userState = Provider.of<UserState>(context);
-    final User user = userState.user!;
+    final User user = watch(userProvider).state!;
+    final messageText = watch(messageTextProvider).state;
 
     return Scaffold(
       appBar: AppBar(
@@ -302,9 +406,11 @@ class _AddPostPageState extends State<AddPostPage> {
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
                 onChanged: (String value) {
-                  setState(() {
+                  // Providerから値を更新
+                  context.read(messageTextProvider).state = value;
+/*                   setState(() {
                     messageText = value;
-                  });
+                  }); */
                 },
               ),
               const SizedBox(
@@ -354,7 +460,7 @@ class CountData extends ChangeNotifier {
   }
 }
 
-class ParentWidget extends StatelessWidget {
+/* class ParentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Provider<T>() で子Widgetにデータを渡す
@@ -368,9 +474,9 @@ class ParentWidget extends StatelessWidget {
       ),
     );
   }
-}
+} */
 
-class ChildWidget extends StatelessWidget {
+/* class ChildWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Provider.of<T>(context) で親Widgetからデータを受け取る
@@ -391,3 +497,4 @@ class ChildWidget extends StatelessWidget {
     );
   }
 }
+ */
